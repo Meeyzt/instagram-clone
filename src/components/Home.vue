b8836ab9df2439e153ecb37505ba000dd6bfca5e
Mehmet Yılmaz <64976979+Meeyzt@users.noreply.github.com>
2021-11-22 17:08:33 +0300

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Helooğğ</div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Heloğğğ
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Home',
+  components: {
+  },
+});
+</script>
