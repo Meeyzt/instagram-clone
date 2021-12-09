9afb46347f285977eee6f866243500809ce3d111
Osman Berk Aşık <osmnbrkask@gmail.com>
2021-12-09 11:44:51 +0300

    Mahmut

---
+++ src/store/pages/home/mutations.ts
@@ -0,0 +1,9 @@
+import { MutationTree } from 'vuex';
+import { HomeState } from './types';
+
+// eslint-disable-next-line import/prefer-default-export
+export const mutations: MutationTree<HomeState> = {
+  setHello(state, payload: string) {
+    state.Hello = payload;
+  },
+}
